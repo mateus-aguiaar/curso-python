bb84d2be7e60176638f0b9c33d462a2fa0511db4
Mateus Davi <vhg.contacts@gmail.com>
2026-04-24 01:42:21 -0300

    adding a new exercise

---
+++ exercises-01-120/world2/042.py
@@ -0,0 +1,16 @@
+comprimento1 = float(input('\nEnter the length of a line: '))
+comprimento2 = float(input('\nEnter the length of a line: '))
+comprimento3 = float(input('\nEnter the length of a line: '))
+
+if comprimento1 < comprimento2 + comprimento3 and comprimento2 < comprimento1 + comprimento3 and comprimento3 < comprimento1 + comprimento2:
+    print('\nIt is possible to form a triangle')
+    if comprimento1 == comprimento2 == comprimento3:
+        print("\nequilateral triangle ")
+
+    elif comprimento1 == comprimento2 != comprimento3 or comprimento2 == comprimento3 != comprimento1 or comprimento1 == comprimento3 != comprimento2:
+        print("\nIsosceles triangle")
+     
+    else:
+        print("\nScalene triangle")
+else:
+    print('It is not possible to form a triangle')
